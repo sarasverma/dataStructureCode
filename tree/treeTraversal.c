47bd359b4cbdb243c65f9be4816ea4a8b76ede77
Saras <78587510+sarasverma@users.noreply.github.com>
2021-12-19 20:25:46 +0530

    Add files via upload

---
+++ tree/treeTraversal.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void preorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        // root left right
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        // left right root
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        // left root right
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    // create node
+    struct node *root = createNode(3);
+    struct node *node2 = createNode(12);
+    struct node *node3 = createNode(5);
+    struct node *node4 = createNode(8);
+    struct node *node5 = createNode(9);
+
+    // make it point each others
+    root->left = node2;
+    root->right = node3;
+
+    node2->left = node4;
+    node2->right = node5;
+
+    // my tree structue
+    //      r
+    //     / \
+    //   n2  n3
+    //   /\
+    // n4 n5
+
+    // other representation for tree
+    //      r
+    //     / \
+    //   n2  n3
+    //   /\
+    // n4 n5
+    preorder(root);
+    printf("\n");
+
+    postorder(root);
+    printf("\n");
+
+    inorder(root);
+    printf("\n");
+
+    return 0;
+}
